403955222e1fc2a09111ebd4ad312184bd423db1
geyunxiang <geyunxiang_95@sina.com>
2018-01-18 14:31:33 +0800

    put result utils code to functions and add plotting overlap histogram

--- mmdps/utils/result_utils.py
+++ mmdps/utils/result_utils.py
@@ -15,29 +15,33 @@ def generate_net_heatmap(net, output_file, title):
 	plt.savefig(output_file)
 	plt.close()
 
-class HistGenerator:
-	def __init__(self, boldPath, template_name, resultPath):
-		self.boldPath = boldPath
-		self.template = brain_template.get_template(template_name)
-		self.resultPath = resultPath # 'E:/Results/Beijing Zang FC Analysis/'
+def get_all_fc_at_ticks(xtick, ytick, boldPath, template_name):
+	ret = []
+	template = brain_template.get_template(template_name)
+	for scan in os.listdir(boldPath):
+		matpath = os.path.join(boldPath, scan, 'bold_net/brodmann_lr_3/corrcoef.csv')
+		rawnet = load_csvmat(matpath)
+		ret.append(rawnet[template.ticks_to_indexes([xtick])[0], template.ticks_to_indexes([ytick])[0]])
+	return ret
 
-	def get_all_fc_at_ticks(self, xtick, ytick):
-		ret = []
-		for scan in os.listdir(self.boldPath):
-			matpath = os.path.join(self.boldPath, scan, 'bold_net/brodmann_lr_3/corrcoef.csv')
-			rawnet = load_csvmat(matpath)
-			ret.append(rawnet[self.template.ticks_to_indexes([xtick])[0], self.template.ticks_to_indexes([ytick])[0]])
-		return ret
-
-	def plot_FCHist_at_tick(self, xtick, ytick, show_img = False):
-		data = self.get_all_fc_at_ticks(xtick, ytick)
-		plt.hist(data, bins = 40, range = (-1, 1))
-		plt.xlabel('functional connectivity')
-		plt.ylabel('num')
-		plt.title('fc hist %s-%s' % (xtick, ytick))
-		os.makedirs(os.path.join(self.resultPath, 'FC Hist'), exist_ok = True)
+def plot_FCHist_at_tick(xtick, ytick, boldPath, template_name, save_to_file = None, show_img = False):
+	data = get_all_fc_at_ticks(xtick, ytick, boldPath, template_name)
+	plt.hist(data, bins = 40, range = (-1, 1))
+	plt.xlabel('functional connectivity')
+	plt.ylabel('num')
+	plt.title('fc hist %s-%s' % (xtick, ytick))
+	if save_to_file:
+		os.makedirs(os.path.dirname(save_to_file), exist_ok = True)
 		plt.savefig(os.path.join(self.resultPath, 'FC Hist/%s-%s fc hist.png' % (xtick, ytick)))
-		if show_img:
-			plt.show()
-		else:
-			plt.close()
+	if show_img:
+		plt.show()
+	else:
+		plt.close()
+
+def overlap_FCHists_at_tick(xtick, ytick, boldPaths, names, template_name, save_to_file = None, show_img = False):
+	alpha_value = 1.0/len(boldPaths)
+	for boldPath, name in zip(boldPaths, names):
+		data = get_all_fc_at_ticks(xtick, ytick, boldPath, template_name)
+		plt.hist(data, bins = 40, range = (-1, 1), alpha = alpha_value, label = name)
+	plt.legend(loc = 'upper right')
+	plt.show()
