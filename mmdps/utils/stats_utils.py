1b0d1072a98e35150f4a0f4e64a3f3fa4bded2c6
geyunxiang <geyunxiang_95@sina.com>
2018-04-11 19:22:44 +0800

    update sigdiff filter funcion

--- mmdps/utils/stats_utils.py
+++ mmdps/utils/stats_utils.py
@@ -27,10 +27,13 @@ def filter_sigdiff_connections(netListA, netListB, sigLevel = 0.05):
 	"""
 	ret = []
 	sampleNet = netListA[0]
+	totalTests = 0
 	for xidx in range(len(sampleNet.ticks)):
 		for yidx in range(xidx + 1, len(sampleNet.ticks)):
 			t, p = scipy.stats.ttest_ind([a.get_value_at_idx(xidx, yidx) for a in netListA], 
 				[b.get_value_at_idx(xidx, yidx) for b in netListB])
+			totalTests += 1
 			if p < sigLevel:
 				ret.append('%s-%s' % (sampleNet.ticks[xidx], sampleNet.ticks[yidx]))
+	print('SigDiff connections: %d. Discover rate: %1.4f with sigLevel: %1.4f' % (len(ret), float(len(ret))/totalTests, sigLevel))
 	return ret
