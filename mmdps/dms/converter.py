b750ae40293e1ca9ed0065896266da3e3bd99e18
geyunxiang <geyunxiang_95@sina.com>
2018-10-31 16:16:31 +0800

    update import workflow

--- mmdps/dms/converter.py
+++ mmdps/dms/converter.py
@@ -1,8 +1,9 @@
-"""Converter for dicom and nifti import.
+"""
+Converter for dicom and nifti import.
 
 Convert dicom to nifti. 
-Get the main modals use NiftiGetter, which is based on globbing.
-Inherite NiftiGetter for use with specific converted raw nifti data.
+Get the main modals using NiftiGetter, which is based on globbing.
+Inherite NiftiGetter to use with specific converted raw nifti data.
 """
 
 import os
@@ -19,96 +20,93 @@ from mmdps.util import path, loadsave
 import dicominfo
 
 def gen_scan_info(infolder, outfolder):
-    """Generate scan_info.json file from dicom files.
+	"""
+	Generate scan_info.json file from dicom files.
 
-    Use one random dicom file.
-    """
-    found = False
-    for dirpath, dirnames, filenames in os.walk(infolder):
-        if not found:
-            for filename in filenames:
-                try:
-                    dicom.read_file(os.path.join(dirpath, filename))
-                    found = True
-                except:
-                    pass
-                if found:
-                    di = dicominfo.DicomInfo(os.path.join(dirpath, filename))
-                    d = di.get_scan_info()
-                    scaninfofile = os.path.join(outfolder, 'scan_info.json')
-                    loadsave.save_json_ordered(scaninfofile, d)
+	Use one random dicom file.
+	"""
+	found = False
+	for dirpath, dirnames, filenames in os.walk(infolder):
+		if not found:
+			for filename in filenames:
+				try:
+					dicom.read_file(os.path.join(dirpath, filename))
+					found = True
+				except:
+					pass
+				if found:
+					di = dicominfo.DicomInfo(os.path.join(dirpath, filename))
+					d = di.get_scan_info()
+					scanInfoFile = os.path.join(outfolder, 'scan_info.json')
+					loadsave.save_json_ordered(scanInfoFile, d)
 
-    
 def convert_dicom_to_nifti(infolder, outfolder):
-    """Convert DICOM to raw NIFTI.
-    
-    The infolder should be the DICOM folder.
-    The outfolder will be the converted NIFTI folder.
-    the ret is the conversion program return value. 0 typically indicates success.
-    """
-    path.rmtree(outfolder)
-    path.makedirs(outfolder)
-    gen_scan_info(infolder, outfolder)
-    ret = subprocess.call([rootconfig.path.dcm2nii, '-z', 'y', '-o', outfolder, infolder],
-        cwd=os.path.dirname(rootconfig.path.dcm2nii))
-    print(outfolder, ret)
-    return ret
-
+	"""
+	Convert DICOM to raw NIFTI.
+	
+	The infolder should be the DICOM folder.
+	The outfolder will be the converted NIFTI folder.
+	the ret is the conversion program return value. 0 typically indicates success.
+	"""
+	path.rmtree(outfolder)
+	path.makedirs(outfolder)
+	gen_scan_info(infolder, outfolder)
+	ret = subprocess.call([rootconfig.path.dcm2nii, '-z', 'y', '-o', outfolder, infolder],
+		cwd=os.path.dirname(rootconfig.path.dcm2nii))
+	print(outfolder, ret)
+	return ret
 
 class NiftiGetter:
-    """Get specific modal from converted raw nii files."""
-    def __init__(self, niftifolder):
-        """Init with the folder that contains nii files."""
-        self.niftifolder = niftifolder
-        self._files = os.listdir(self.niftifolder)
+	"""Get specific modal from converted raw nii files."""
+	def __init__(self, niftifolder):
+		"""Init with the folder that contains nii files."""
+		self.niftifolder = niftifolder
+		self._files = os.listdir(self.niftifolder)
 
-    def fnmatch_all(self, pat):
-        """Match all files that match the pattern."""
-        res = []
-        for file in self._files:
-            if fnmatch.fnmatch(file, pat):
-                res.append(os.path.join(self.niftifolder, file))
-        return res
-    
-    def fnmatch_one(self, pat):
-        """Match exactly one file with pattern."""
-        res = self.fnmatch_all(pat)
-        if len(res) == 1:
-            return res[0]
-        elif len(res) == 0:
-            logging.warning('No file match: {}: {}'.format(pat, self.niftifolder))
-            print('No file match:', pat)
-            return None
-        else:
-            logging.warning('More than one match: {} {}: {}'.format(pat, res, self.niftifolder))
-            print('More than one match:', pat, res)
-            return None
-        
-    def get_T1(self):
-        """Get T1 NIFTI file path."""
-        return self.fnmatch_one('*T1*.nii.gz')
+	def fnmatch_all(self, pat):
+		"""Match all files that match the pattern."""
+		res = []
+		for file in self._files:
+			if fnmatch.fnmatch(file, pat):
+				res.append(os.path.join(self.niftifolder, file))
+		return res
 
-    def get_T2(self):
-        """Get T2 NIFTI file path."""
-        return self.fnmatch_one('*T2*.nii.gz')
+	def fnmatch_one(self, pat):
+		"""Match exactly one file with pattern."""
+		res = self.fnmatch_all(pat)
+		if len(res) == 1:
+			return res[0]
+		elif len(res) == 0:
+			logging.warning('No file match: {}: {}'.format(pat, self.niftifolder))
+			print('No file match:', pat)
+			return None
+		else:
+			logging.warning('More than one match: {} {}: {}'.format(pat, res, self.niftifolder))
+			print('More than one match:', pat, res)
+			return None
 
-    def get_BOLD(self):
-        """Get BOLD NIFTI file path."""
-        return self.fnmatch_one('*BOLD*.nii.gz')
+	def get_T1(self):
+		"""Get T1 NIFTI file path."""
+		return self.fnmatch_one('*T1*.nii.gz')
 
-    def get_DWI(self):
-        """Get DWI NIFTI file, bval file and bvec file, in a tuple.
-        
-        Validate with all(dwifiles) == True
-        """
-        nii = self.fnmatch_one('*DWI*.nii.gz')
-        bval = self.fnmatch_one('*DWI*.bval')
-        bvec = self.fnmatch_one('*DWI*.bvec')
-        return (nii, bval, bvec)
+	def get_T2(self):
+		"""Get T2 NIFTI file path."""
+		return self.fnmatch_one('*T2*.nii.gz')
 
-    def get_ScanInfo(self):
-        """Get scan info dict."""
-        return os.path.join(self.niftifolder, 'scan_info.json')
-    
-    
-        
+	def get_BOLD(self):
+		"""Get BOLD NIFTI file path."""
+		return self.fnmatch_one('*BOLD*.nii.gz')
+
+	def get_DWI(self):
+		"""Get DWI NIFTI file, bval file and bvec file, in a tuple.
+		
+		Validate with all(dwifiles) == True
+		"""
+		nii = self.fnmatch_one('*DWI*.nii.gz')
+		bval = self.fnmatch_one('*DWI*.bval')
+		bvec = self.fnmatch_one('*DWI*.bvec')
+		return (nii, bval, bvec)
+
+	def get_ScanInfo(self):
+		"""Get scan info dict."""
+		return os.path.join(self.niftifolder, 'scan_info.json')
--- tools/import_changgung.py
+++ tools/import_changgung.py
@@ -10,102 +10,97 @@ from mmdps.util import clock
 logging.basicConfig(filename='import_changgung.log', level=logging.DEBUG)
 logging.info('New Run: {}'.format(clock.now()))
 
-class ChanggungNiftiGetter(converter.NiftiGetter):
-    def __init__(self, niftifolder):
-        super().__init__(niftifolder)
-
-    def get_T1(self):
-        return self.fnmatch_one('*OSag_3D_T1BRAVO*.nii.gz')
-
-    def get_T2(self):
-        return self.fnmatch_one('*OAx_T2_PROPELLER*.nii.gz')
-
-    def get_BOLD(self):
-        return self.fnmatch_one('*BOLD-rest*.nii.gz')
-
-    def get_DWI(self):
-        nii = self.fnmatch_one('*DTI_24_Directions*.nii.gz')
-        bval = self.fnmatch_one('*DTI_24_Directions*.bval')
-        bvec = self.fnmatch_one('*DTI_24_Directions*.bvec')
-        dwifiles = (nii, bval, bvec)
-        if all(dwifiles):
-            return dwifiles
-        else:
-            return None
-    
 def apppath(s):
-    return os.path.join(WORKING_FOLDER, s)
+	return os.path.join(WORKING_FOLDER, s)
+
+class ChanggungNiftiGetter(converter.NiftiGetter):
+	def __init__(self, niftifolder):
+		super().__init__(niftifolder)
+
+	def get_T1(self):
+		return self.fnmatch_one('*OSag_3D_T1BRAVO*.nii.gz')
+
+	def get_T2(self):
+		return self.fnmatch_one('*OAx_T2_PROPELLER*.nii.gz')
+
+	def get_BOLD(self):
+		return self.fnmatch_one('*BOLD-rest*.nii.gz')
+
+	def get_DWI(self):
+		nii = self.fnmatch_one('*DTI_24_Directions*.nii.gz')
+		bval = self.fnmatch_one('*DTI_24_Directions*.bval')
+		bvec = self.fnmatch_one('*DTI_24_Directions*.bvec')
+		dwifiles = (nii, bval, bvec)
+		if all(dwifiles):
+			return dwifiles
+		else:
+			return None
 
 class ChanggungConverter:
-    def __init__(self, dcmmainfolder, niftimainfolder, mriscantxt):
-        self.dcmmainfolder = dcmmainfolder
-        self.niftimainfolder = niftimainfolder
-        self.mriscans = load_txt(mriscantxt)
+	def __init__(self, dcmMainFolder, niftiMainFolder, mriscantxt):
+		self.dcmMainFolder = dcmMainFolder
+		self.niftiMainFolder = niftiMainFolder
+		self.mriscans = load_txt(mriscantxt)
 
-    def convert_mriscan(self, mriscan):
-        dcmfolder = os.path.join(self.dcmmainfolder, mriscan)
-        niftifolder = os.path.join(self.niftimainfolder, mriscan)
-        ret = converter.convert_dicom_to_nifti(dcmfolder, niftifolder)
-        return ret
-        
-    def run(self):
-        for mriscan in self.mriscans:
-            ret = self.convert_mriscan(mriscan)
-            if ret == 0:
-                logging.info('{} convert completed'.format(mriscan))
-            else:
-                logging.warning('{} convert failed, {}'.format(mriscan, ret))
-            
+	def convert_mriscan(self, mriscan):
+		dcmfolder = os.path.join(self.dcmMainFolder, mriscan)
+		niftifolder = os.path.join(self.niftiMainFolder, mriscan)
+		ret = converter.convert_dicom_to_nifti(dcmfolder, niftifolder)
+		return ret
+
+	def run(self):
+		for mriscan in self.mriscans:
+			ret = self.convert_mriscan(mriscan)
+			if ret == 0:
+				logging.info('{} convert completed'.format(mriscan))
+			else:
+				logging.warning('{} convert failed, {}'.format(mriscan, ret))
+
 if __name__ == '__main__':
-    print('...')
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--mriscanstxt', help='a txt file contain mriscan folder names.', required=True, default='')
-    parser.add_argument('--bconvert', help='convert dicom to rawnii or not', default='True')
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--mriscanstxt', help='a txt file containing mriscan folder names.', required=True, default='')
+	parser.add_argument('--bconvert', help='convert dicom to rawnii or not', default='True')
+	args = parser.parse_args()
 
-    mriscanstxt = args.mriscanstxt
-    
-    if args.mriscanstxt == '':
-        # change this if with out arguments
-        mriscanstxt = r'F:\MMDPDatabase\new_data_dicom\changgeng_20180430.txt'
-        
-    # do dcm2nii or not
-    bconvert = args.bconvert == 'True'
+	mriscanstxt = args.mriscanstxt
+	if args.mriscanstxt == '':
+		# change this when running without arguments
+		mriscanstxt = r'F:\MMDPDatabase\new_data_dicom\changgeng_20180430.txt'
 
-    print(mriscanstxt, bconvert)
+	# perform dcm2nii or not
+	bconvert = args.bconvert == 'True'
 
+	print('Running with scan list: %s and convertion switch: %s' % (mriscanstxt, bconvert))
 
-    # Working folder
-    WORKING_FOLDER = rootconfig.dms.folder_working
-    # DICOM main folder
-    indcmmainfolder = rootconfig.dms.folder_dicom
-    # Raw NIfTI main folder
-    inoutniftimainfolder = rootconfig.dms.folder_rawnii
-    # MRIData main folder
-    outmainfolder = rootconfig.dms.folder_mridata
+	# Working folder
+	WORKING_FOLDER = rootconfig.dms.folder_working
+	# DICOM main folder
+	indcmMainFolder = rootconfig.dms.folder_dicom
+	# Raw NIfTI main folder
+	outniftiMainFolder = rootconfig.dms.folder_rawnii
+	# MRIData main folder
+	outMainFolder = rootconfig.dms.folder_mridata
 
-    if bconvert:
-        cvt = ChanggungConverter(indcmmainfolder, inoutniftimainfolder, mriscanstxt)
-        cvt.run()
-    
-    csv_motionscores = None
-    csv_strokescores = None
-    groupconflist = []
-##    csv_motionscores = apppath('rawtable/scipatient_motionscores.csv')
-##    csv_strokescores = apppath('rawtable/jixieshou_BCI+Jixieshou_scores.csv')
-##    groupconflist = [('normal', 'Healthy normal people.', apppath('rawtable/normal_peopleorder.txt')),
-##         ('scipatient', 'SCI patients.', apppath('rawtable/patient_peopleorder.txt')),
-##         ('strokepatient', 'Stroke patients.', apppath('rawtable/jixieshou.txt'))]
+	if bconvert:
+		cvt = ChanggungConverter(indcmMainFolder, outniftiMainFolder, mriscanstxt)
+		cvt.run()
 
-    db = dbgen.DatabaseGenerator(
-        apppath('rawtable/ChanggengMRITable.csv'),
-        apppath('mmdpdb.db'),
-        csv_motionscores,
-        csv_strokescores,
-        groupconflist
-        )
-    cls_niftigetter = ChanggungNiftiGetter
-    imp = importer.MRIScanImporter(inoutniftimainfolder, outmainfolder, mriscanstxt, db, cls_niftigetter)
-    imp.run()
+	csv_motionscores = None
+	csv_strokescores = None
+	groupconflist = []
+	##    csv_motionscores = apppath('rawtable/scipatient_motionscores.csv')
+	##    csv_strokescores = apppath('rawtable/jixieshou_BCI+Jixieshou_scores.csv')
+	##    groupconflist = [('normal', 'Healthy normal people.', apppath('rawtable/normal_peopleorder.txt')),
+	##         ('scipatient', 'SCI patients.', apppath('rawtable/patient_peopleorder.txt')),
+	##         ('strokepatient', 'Stroke patients.', apppath('rawtable/jixieshou.txt'))]
 
-    
+	db = dbgen.DatabaseGenerator(
+		apppath('rawtable/ChanggengMRITable.csv'),
+		apppath('mmdpdb.db'),
+		csv_motionscores,
+		csv_strokescores,
+		groupconflist
+		)
+	cls_niftigetter = ChanggungNiftiGetter
+	imp = importer.MRIScanImporter(outniftiMainFolder, outMainFolder, mriscanstxt, db, cls_niftigetter)
+	imp.run()
