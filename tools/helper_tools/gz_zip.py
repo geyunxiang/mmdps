8f60cc87a380de142054324cad6fab2e5eac3d24
geyunxiang <geyunxiang_95@sina.com>
2020-04-22 11:01:02 +0800

    Update mmdps helper tools

--- tools/helper_tools/gz_zip.py
+++ tools/helper_tools/gz_zip.py
@@ -1,22 +1,14 @@
 import sys
-import gzip
-import shutil
 
 from mmdps.util.fileop import gz_zip
 
-    
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python gunzip thefile.gz')
-        sys.exit(-1)
-        
-    gzfile = sys.argv[1]
-    
-    if gzfile[-3:] != '.gz':
-        print('The file must end with .gz')
-        sys.exit(-2)
-        
-    b = gz_zip(gzfile)
-    if b == None:
-        sys.exit(-3)
-        
+	if len(sys.argv) != 2:
+		print('Usage: python gz_zip.py file')
+		sys.exit(-1)
+
+	gzfile = sys.argv[1]
+
+	b = gz_zip(gzfile)
+	if b is None:
+		sys.exit(-3)
